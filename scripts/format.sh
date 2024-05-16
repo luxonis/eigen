34b2b6b7f8d97897864d2a94385d8a518eedc85d
Tyler Veness <calcmogul@gmail.com>
2024-05-16 12:55:56 -0700

    Format .cc and .cu files

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find . -type f \( -name '*.cpp' -o -name '*.h' -o -name '*.inc' \) | xargs -n 1 -P 0 clang-format -i
+find . -type f \( -name '*.cc' -o -name '*.cpp' -o -name '*.cu' -o -name '*.h' -o -name '*.inc' \) | xargs -n 1 -P 0 clang-format -i
 find Eigen -maxdepth 1 -type f | xargs -n 1 -P 0 clang-format -i
 find unsupported/Eigen -maxdepth 1 -type f -not -name '*.txt' | xargs -n 1 -P 0 clang-format -i
