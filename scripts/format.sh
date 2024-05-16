5e49c2b02c7b0b8e31a971fa34fd8151af528181
Tyler Veness <calcmogul@gmail.com>
2024-05-16 13:15:38 -0700

    Format .c and .cxx files too

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Format files with extensions, excluding plugins because they're partial files that don't contain valid syntax
-find . -type f \( -name '*.cc' -o -name '*.cpp' -o -name '*.cu' -o -name '*.h' -o -name '*.inc' -not -path '*/plugins/*' \) | xargs -n 1 -P 0 clang-format -i
+find . -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.cu' -o -name '*.cxx' -o -name '*.h' -o -name '*.inc' -not -path '*/plugins/*' \) | xargs -n 1 -P 0 clang-format -i
 
 # Format main headers without extensions
 find Eigen -maxdepth 1 -type f | xargs -n 1 -P 0 clang-format -i
