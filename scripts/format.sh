bde477259a939fe92ab38c1fc9690dc44c17595b
Tyler Veness <calcmogul@gmail.com>
2024-05-16 12:47:46 -0700

    Use xargs instead

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find . -type f \( -name '*.cpp' -o -name '*.h' -o -name '*.inc' \) | parallel clang-format -i {}
-find Eigen -maxdepth 1 -type f | parallel clang-format -i {}
-find unsupported/Eigen -maxdepth 1 -type f -not -name '*.txt' | parallel clang-format -i {}
+find . -type f \( -name '*.cpp' -o -name '*.h' -o -name '*.inc' \) | xargs -n 1 -P 0 clang-format -i
+find Eigen -maxdepth 1 -type f | xargs -n 1 -P 0 clang-format -i
+find unsupported/Eigen -maxdepth 1 -type f -not -name '*.txt' | xargs -n 1 -P 0 clang-format -i
